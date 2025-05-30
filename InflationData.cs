5c6a1183b75f98225d0128a3bf1d9209cee911d7
GomerSec <ggoomeerr@gmail.com>
2025-05-30 11:07:10 +0300

    Добавлен класс для хранения данных об инфляции
    Два свойства: год, процент инфляции

---
+++ InflationData.cs
@@ -0,0 +1,12 @@
+using Microsoft.Win32;
+using System.Windows;
+using System.Collections.Generic;
+
+namespace laba3
+{
+    public class InflationData
+  {
+      public int Year { get; set; }
+      public double InflationRate { get; set; }
+  }
+}
